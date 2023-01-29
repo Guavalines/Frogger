ddd85ad2ac53ee71f6c77b21eb538a99aa1a60ed
guavalines <konstrictor@gmail.com>
2023-01-28 21:25:05 -0800

    selected all squares in grid

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const timeLeftDisplay = document.querySelector('#time-left')
 const resultDisplay = document.querySelector('#result')
 const StartPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')

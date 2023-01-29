b0d7ad84bdfeffbf9f14b651e3e33239e13e427f
guavalines <konstrictor@gmail.com>
2023-01-29 01:57:11 -0800

    added frog starting block

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const timeLeftDisplay = document.querySelector('#time-left')
 const resultDisplay = document.querySelector('#result')
 const StartPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
-
+console.log(squares)
+let currentIndex = 76
 function moveFrog() {
-  console.log('moved')
+
+
+  squares[currentIndex].classList.add('frog')
 }
 document.addEventListener('keyup', moveFrog)

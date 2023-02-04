22dbf3bebee4e4d60712ccb7581c502a11cf98cd
guavalines <konstrictor@gmail.com>
2023-02-03 23:04:12 -0800

    modified move log right

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const resultDisplay = document.querySelector('#result')
 const StartPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
 const logsleft = document.querySelectorAll('.log-left')
+const logsright = document.querySelectorAll('.log-right')
+
 console.log(squares)
 let currentIndex = 76
 const width = 9
@@ -32,6 +34,7 @@ document.addEventListener('keyup', moveFrog)
 
 function autoMoveLogs() {
   logsleft.forEach(logLeft => moveLogLeft(logLeft))
+  logsright.forEach(logRight => moveLogRight(logRight))
 }
 
 function moveLogLeft(logLeft) {
@@ -59,27 +62,27 @@ function moveLogLeft(logLeft) {
   }
 }
 
-function moveLogRight(logLeft) {
+function moveLogRight(logRight) {
   switch(true) {
-    case logLeft.classList.contains('l1') :
-      logLeft.classList.remove('l1')
-      logLeft.classList.add('l2')
+    case logRight.classList.contains('l1') :
+      logRight.classList.remove('l1')
+      logRight.classList.add('l2')
       break
-    case logLeft.classList.contains('l2') :
-      logLeft.classList.remove('l2')
-      logLeft.classList.add('l3')
+    case logRight.classList.contains('l2') :
+      logRight.classList.remove('l2')
+      logRight.classList.add('l3')
       break
-    case logLeft.classList.contains('l3') :
-      logLeft.classList.remove('l3')
-      logLeft.classList.add('l4')
+    case logRight.classList.contains('l3') :
+      logRight.classList.remove('l3')
+      logRight.classList.add('l4')
       break
-    case logLeft.classList.contains('l4') :
-      logLeft.classList.remove('l4')
-      logLeft.classList.add('l5')
+    case logRight.classList.contains('l4') :
+      logRight.classList.remove('l4')
+      logRight.classList.add('l5')
       break
-    case logLeft.classList.contains('l5') :
-      logLeft.classList.remove('l5')
-      logLeft.classList.add('l1')
+    case logRight.classList.contains('l5') :
+      logRight.classList.remove('l5')
+      logRight.classList.add('l1')
       break
   }
 }

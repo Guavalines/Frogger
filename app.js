132b4a1f0a494d26f26edcf1ad4b57ee8913829d
guavalines <konstrictor@gmail.com>
2023-02-06 12:51:44 -0800

    added move car right function

--- app.js
+++ app.js
@@ -108,4 +108,21 @@ function moveCarLeft(carLeft) {
   }
 }
 
+function moveCarRight(carRight) {
+  switch(true) {
+    case carRight.classList.contains('c1') :
+      carRight.classList.remove('c1')
+      carRight.classList.add('c3')
+      break
+    case carRight.classList.contains('c2') :
+      carRight.classList.remove('c2')
+      carRight.classList.add('c1')
+      break
+    case carRight.classList.contains('c3') :
+      carRight.classList.remove('c3')
+      carRight.classList.add('c2')
+      break
+  }
+}
+
 setInterval(autoMoveElements, 1000)
